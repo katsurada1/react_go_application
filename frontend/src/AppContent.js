61824ecca5549c50e29d14c9177f8fe4b5e03f22
kasturada1 <kazuki1.katsurada@gmail.com>
2023-04-28 20:36:47 +0900

    Add fetchList and render json

--- frontend/src/AppContent.js
+++ frontend/src/AppContent.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 
 export default class AppContent extends Component {
+  anotherFunction = () => {
+    console.log('another function');
+  };
+
+  leftParagraph = () => {
+    console.log('left the paragraph');
+  };
+
+  fetchList = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.anotherFunction();
+        let posts = document.getElementById('post-list');
+
+        json.forEach(function (obj) {
+          let li = document.createElement('li');
+          li.appendChild(document.createTextNode(obj.title));
+          posts.appendChild(li);
+        });
+      });
+  };
+
   render() {
     return (
       <p>
         This is the content.
         <br />
+        <hr />
+        <p
+          onMouseEnter={this.anotherFunction}
+          onMouseLeave={this.leftParagraph}
+        >
+          This is some text
+        </p>
+        <button onClick={this.fetchList} className="btn btn-primary">
+          Fetch Data
+        </button>
+        <hr />
+        <ul id="post-list"></ul>
       </p>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import AppFooter from './AppFooter';
-import './index.css';
-import AppContent from './AppContent';
 import AppHeader from './AppHeader';
+import AppFooter from './AppFooter';
+import AppContent from './AppContent';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
 
 class App extends Component {
   render() {
     const myProps = {
-      title: 'My cool App!',
+      title: 'My Cool App!',
       subject: 'My subject',
       favourite_color: 'red',
     };
